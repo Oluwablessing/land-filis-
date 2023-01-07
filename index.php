4d2587e93ae1e44d51c0c643a15dc61631043c85
Oluwablessing <97201562+Oluwablessing@users.noreply.github.com>
2023-01-07 06:56:27 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="cssdoc/animations.css">  
+    <link rel="stylesheet" href="cssdoc/main.css">  
+    <link rel="stylesheet" href="cssdoc/index.css">
+    <title>Safehand Home page</title>
+    <style>
+        table{
+            animation: transitionIn-Y-bottom 0.5s;
+        }
+    </style>
+        
+</head>
+<body>
+    
+    <div class="full-height">
+        <center>
+        <table border="0">
+            <tr>
+                <td width="80%">
+                    <font class="edoc-logo">Safehand. </font>
+                    <font class="edoc-logo-sub">| THE BANK OF SAVINGS, LOANING AND INVESTING</font>
+                </td>
+                <td width="10%">
+                   <a href="chat.php"  class="non-style-link"><p class="nav-item">Chat us live</p></a>
+                </td>
+                <td  width="10%">
+                    <a href="about.php" class="non-style-link"><p class="nav-item" style="padding-right: 10px;">About us</p></a>
+                </td>
+            </tr>
+            
+            <tr>
+                <td  colspan="3">
+                    <p class="heading-text">WELCOME TO SAFEHAND</p>
+
+                </td>
+            </tr>
+            <tr>
+                <td  colspan="3">
+                    <p class="sub-text2">How do  you manage your income, are you still challenged with savings or loaning??
+                        <br>Safehand offers you a quick loan without high rate of charges, of which you can also invest part of your <br>
+                    income to cashout big amount in a later day.<br>
+                    Register with safehand now and freeze your income from wastage.</p>
+                </td>
+            </tr>
+            <tr>
+                
+                <td colspan="3">
+                    <center>
+                    <a href="login.php" >
+                        <input type="button" value="Login Now" class="login-btn btn-primary btn" style="padding-left: 25px;padding-right: 25px;padding-top: 10px;padding-bottom: 10px;">
+                    </a>
+                </center>
+                </td>
+                
+            </tr>
+            <tr>
+                <td colspan="3">
+                   
+                </td>
+            </tr>
+        </table>
+        <p class="sub-text2 footer-hashen">A saved income is a saved dream</p>
+    </center>
+    
+    </div>
+</body>
+</html>
